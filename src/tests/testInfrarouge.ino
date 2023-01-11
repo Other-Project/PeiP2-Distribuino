022b4adc5c48f3c64b9576588177c88a707ae1b9
TeoBaillot <79744829+TeoBaillot@users.noreply.github.com>
2023-01-11 15:20:14 +0100

    Add files via upload

--- src/tests/testInfrarouge.ino
+++ src/tests/testInfrarouge.ino
@@ -12,13 +12,12 @@ void setup(){
 
 void loop(){
   // On teste si le capteur infrarouge détecte un passage 
-  // et on s'assure qu'aucun objet n'ait été détecté moins de 100millisecondes plus tôt.
-  // Cela permet d'éviter qu'une bille passant à faible vitesse devant le capteur comptabilise plusieurs passages
+  // On vérifie qu'aucun objet n'ait été détecté moins de 100millisecondes plus tôt.
   if (digitalRead(INFRAROUGE1)==LOW && millis()-dernierPassage>=100){ 
     Serial.println("pièce détectée");
     nombrePieces+=1; // On incrémente le nombre de passages détectés par le capteur
-    dernierPassage = millis(); // Et on sauvegarde le temps écoulé entre le lancement du programme et ce passage
-    Serial.print("Nombre de pièces :"); // Finalement on affiche le nombre de passages sur le moniteur série
+    dernierPassage = millis(); // On sauvegarde le temps écoulé entre le lancement du programme et ce passage
+    Serial.print("Nombre de pièces :"); // On affiche le nombre de passages sur le moniteur série
     Serial.println(nombrePieces);
   }
 }
